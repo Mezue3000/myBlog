0ed568c495af5b2cadced0663a0c32fc01f9c339
mezue30000 <mezue3000>
2025-05-15 20:03:28 +0100

    created Post model class

--- server/app/models.py
+++ server/app/models.py
@@ -9,6 +9,8 @@ from sqlalchemy.orm import Mapped
 
 # create user model
 class User(SQLModel, table=True):
+    __tablename__ = "users"
+    
     user_id: Optional[int] = Field(default=None, primary_key=True)
     first_name: str = Field(max_length=25, nullable=False)
     last_name: str = Field(max_length=25, nullable=False)
@@ -18,3 +20,24 @@ class User(SQLModel, table=True):
     country: str = Field(max_length=25, nullable=False)
     city: str = Field(max_length=25, nullable=False)
     created_at: datetime = Field(default_factory = lambda: datetime.now(timezone.utc), nullable=False)
+    
+    
+
+
+
+# create post model
+class Post(SQLModel, table=True):
+    __tablename__ = "posts"
+    
+    post_id: Optional[str] = Field(default_factory=None, primary_key=True)
+    title: str = Field(max_length=125, nullable=False, index=True)
+    content: str = Field(max_length=450, nullable=False)
+    created_at: datetime = Field(default_factory = lambda: datetime.now(timezone.utc), nullable=False)
+    updated_at: datetime = Field(
+        default_factory = lambda: datetime.now(timezone.utc), 
+        sa_column_kwargs= {"onupdate": func.now()}, nullable=False)
+    # add foreign key
+    user_id: int = Field(foreign_key = "user.user_id")
+        
+    
+    
