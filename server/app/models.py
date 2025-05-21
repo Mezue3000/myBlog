42644c6c23801c98ad598468e74810e6109ec7d0
mezue30000 <mezue3000>
2025-05-21 21:59:10 +0100

    modified models

--- server/app/models.py
+++ server/app/models.py
@@ -1,7 +1,6 @@
 # import dependencies
-from sqlmodel import SQLModel, Field, Relationship, func
+from sqlmodel import SQLModel, Field, Relationship, func, Index
 from typing import Optional, List
-from pydantic import EmailStr
 from datetime import datetime, timezone
 from sqlalchemy.orm import Mapped
 
@@ -15,7 +14,7 @@ class User(SQLModel, table=True):
     first_name: str = Field(max_length=25, nullable=False)
     last_name: str = Field(max_length=25, nullable=False)
     username: str = Field(max_length=75, nullable=False, index=True)
-    email: EmailStr = Field(max_length=75, nullable=False)
+    email: str = Field(max_length=75, nullable=False)
     password_hash: str = Field(max_length=255, nullable=False)
     country: str = Field(max_length=25, nullable=False)
     city: str = Field(max_length=25, nullable=False)
@@ -24,14 +23,14 @@ class User(SQLModel, table=True):
     posts: Mapped[List["Post"]] = Relationship(back_populates = "user")
     comments: Mapped[List["Comment"]] = Relationship(back_populates = "user")
     
-    
+
 
 
 # create post model
 class Post(SQLModel, table=True):
     __tablename__ = "posts"
     
-    post_id: Optional[str] = Field(default_factory=None, primary_key=True)
+    post_id: Optional[int] = Field(default_factory=None, primary_key=True)
     title: str = Field(max_length=125, nullable=False, index=True)
     content: str = Field(max_length=450, nullable=False)
     created_at: datetime = Field(default_factory = lambda: datetime.now(timezone.utc), nullable=False)
@@ -43,19 +42,20 @@ class Post(SQLModel, table=True):
     # create relationship
     user: Mapped["User"] = Relationship(back_populates = "posts")
     comments: Mapped[List["Comment"]] = Relationship(back_populates = "post")
+    
+    __table_args__ = (Index('post_title_idx', 'title', 'content', mysql_prefix='FULLTEXT')) 
+    
 
 
 
 
+# create comment model
 class Comment(SQLModel, table=True):
     __tablename__ = "comments"
     
-    comment_id: Optional[str] = Field(default_factory=None, primary_key=True)
-    content: str = Field(max_length=450, index=True, nullable=False)
+    comment_id: Optional[int] = Field(default_factory=None, primary_key=True)
+    content: str = Field(max_length=225, index=True, nullable=False)
     created_at: datetime = Field(default_factory = lambda: datetime.now(timezone.utc), nullable=False)
-    updated_at: datetime = Field(
-        default_factory = lambda: datetime.now(timezone.utc), 
-        sa_column_kwargs= {"onupdate": func.now()})
     # add foreign key
     post_id: int = Field(foreign_key = "posts.post_id")
     # create relationship
@@ -63,7 +63,6 @@ class Comment(SQLModel, table=True):
     post: Mapped["Post"] = Relationship(back_populates = "comments")
     
     
-
 # rebuild models
 User.model_rebuild()
 Post.model_rebuild()
