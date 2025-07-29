c945d2b810a02051d42e36be5647119eb0154a3e
mezue30000 <mezue3000>
2025-07-29 05:33:56 +0100

    testing testing

--- server/RBAC.sql
+++ server/RBAC.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE IF NOT EXISTS blogs_app;
 CREATE ROLE IF NOT EXISTS "Admin";
 GRANT ALL PRIVILEGES ON blogs_app.* TO "Admin";
-CREATE USER '*********'@'%' IDENTIFIED BY '*************';
+CREATE USER '*********'@'%' IDENTIFIED BY '**************';
 GRANT 'Admin' TO '*********'@'%';
 SET DEFAULT ROLE 'Admin' TO '*********'@'%';
