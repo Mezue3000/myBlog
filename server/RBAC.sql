85ef1a9c2e17b7eee06acdbd69b6ef13ee011b5c
mezue30000 <mezue3000>
2025-07-31 20:31:20 +0100

    generated requirements.txt file

--- server/RBAC.sql
+++
@@ -1,6 +0,0 @@
-CREATE DATABASE IF NOT EXISTS blogs_app;
-CREATE ROLE IF NOT EXISTS "Admin";
-GRANT ALL PRIVILEGES ON blogs_app.* TO "Admin";
-CREATE USER '*********'@'%' IDENTIFIED BY '**************';
-GRANT 'Admin' TO '*********'@'%';
-SET DEFAULT ROLE 'Admin' TO '*********'@'%';
